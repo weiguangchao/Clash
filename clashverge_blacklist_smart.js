de851bb24d80b2041139094119699e293dc50311
wggc <3409885610@qq.com>
2025-09-30 08:51:30 +0800

    更新全局配置

--- clashverge_blacklist_smart.js
+++ clashverge_blacklist_smart.js
@@ -316,7 +316,9 @@ function overwriteOthers(config) {
   };
 
   //////////////////////////////////////////////////////////////
+  config.mode = "rule";
   config.ipv6 = true;
+  config["log-level"] = "info";
   config.sniffer = sniffer;
   config["tcp-concurrent"] = true;
   config["unified-delay"] = true;
@@ -334,6 +336,7 @@ function overwriteOthers(config) {
 
   //////////////////////////////////////////////////////////////
   config["geodata-mode"] = true;
+  config["geodata-loader"] = "standard";
   config["geodata-auto-update"] = true;
   config["geodata-update-interval"] = 24;
   config["geox-url"] = {
