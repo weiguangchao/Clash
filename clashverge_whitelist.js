758b2949344233e659c4cbaf74106cb7249f2eb1
wggc <3409885610@qq.com>
2025-10-12 15:49:31 +0800

    更新

--- clashverge_whitelist.js
+++ clashverge_whitelist.js
@@ -1,4 +1,4 @@
-function main(config) {
+function main(config, profileName) {
   if (!config) {
     return config;
   }
@@ -32,9 +32,9 @@ function overwriteDns(config) {
     "https://dns.alidns.com/dns-query", // 阿里云公共DNS
     "https://doh.pub/dns-query", // 腾讯DNSPod
     "https://doh.360.cn/dns-query", // 360DNS
-    "https://doh.18bit.cn/dns-query", // 18Bit DNS
-    "https://dns.yuguan.xyz/dns-query", // 易安云DNS
-    "https://doh-pure.onedns.net/dns-query", // OneDNS
+    // "https://doh.18bit.cn/dns-query", // 18Bit DNS
+    // "https://dns.yuguan.xyz/dns-query", // 易安云DNS
+    // "https://doh-pure.onedns.net/dns-query", // OneDNS
   ];
 
   const gfwDnsList = [
@@ -60,15 +60,19 @@ function overwriteDns(config) {
     "use-system-hosts": true,
     "use-hosts": true,
     "fake-ip-filter": [
-      "geosite:private",
+      // "geosite:private",
       // 本地主机/设备
-      "+.lan",
-      "+.local",
+      "*.lan",
+      ".local",
+      "*.arpa",
+      "time.*.com",
+      "ntp.*.com",
       // Windows网络出现小地球图标
       "+.msftconnecttest.com",
       "+.msftncsi.com",
       // parsec
-      "+.parsec.app",
+      "stun.parsec.app",
+      // "+.parsec.app",
       // 小米路由器
       "+.miwifi.com",
       // QQ快速登录检测失败
@@ -76,12 +80,13 @@ function overwriteDns(config) {
       "localhost.sec.qq.com",
       // 微信快速登录检测失败
       "localhost.work.weixin.qq.com",
+      "geosite:private", // 兜底
     ],
-    "default-nameserver": [
-      "tls://1.12.12.12:853",
-      "https://233.5.5.5/dns-query",
-    ],
-    nameserver: cnDohList,
+    // "default-nameserver": [
+    //   "tls://1.12.12.12:853",
+    //   "https://233.5.5.5/dns-query",
+    // ],
+    nameserver: [en0Dns],
     // "proxy-server-nameserver": cnDohList,
     // "direct-nameserver": cnDohList,
     // "nameserver-policy": {
@@ -98,7 +103,6 @@ function overwriteDns(config) {
     // },
   };
 
-  //////////////////////////////////////////////////////////////
   config.dns = dns;
 }
 
@@ -152,54 +156,42 @@ function overwriteRules(config) {
       url: "https://ruleset.skk.moe/Clash/ip/reject.txt",
       path: "./sukkaw_ruleset/reject_ip.txt",
     },
-    telegram_ip: {
-      type: "http",
-      behavior: "classical",
-      format: "text",
-      interval: 43200,
-      url: "https://ruleset.skk.moe/Clash/ip/telegram.txt",
-      path: "./sukkaw_ruleset/telegram_ip.txt",
-    },
   };
 
   // 顺序
   // 1.域名规则
   // 2.IP规则
   const rules = [
-    //////// 禁用YouTube Quic ///////////////////////////////
-    "AND,(AND,(DST-PORT,443),(NETWORK,UDP)),(NOT,((GEOSITE,cn))),REJECT",
-    // "GEOSITE,category-ads-all,REJECT",
+    "AND,((DST-PORT,443),(NETWORK,UDP)),REJECT",
     "RULE-SET,reject_non_ip,REJECT",
     "RULE-SET,reject_domainset,REJECT",
     "RULE-SET,reject_extra_domainset,REJECT",
     "RULE-SET,reject_non_ip_drop,REJECT-DROP",
     "RULE-SET,reject_non_ip_no_drop,REJECT",
     //////////////////////////////////////////////////////////////
-    "GEOSITE,github,🚀 节点选择",
-    "GEOSITE,notion,🚀 节点选择",
-    "GEOSITE,figma,🚀 节点选择",
-    "GEOSITE,cursor,🚀 节点选择",
-    "GEOSITE,gfw,🚀 节点选择",
+    "GEOIP,private,DIRECT,no-resolve",
+    "GEOIP,twitter,🚀 节点选择,no-resolve",
+    "GEOIP,cloudflare,🚀 节点选择,no-resolve",
+    "GEOIP,google,🚀 节点选择,no-resolve",
+    "GEOIP,telegram,🚀 节点选择,no-resolve",
+    "DOMAIN-SUFFIX,xn--ngstr-lra8j.com,DIRECT",
+    "DOMAIN-SUFFIX,googleapis.cn,DIRECT",
     "GEOSITE,private,DIRECT",
-    "GEOSITE,category-public-tracker,DIRECT",
+    "GEOSITE,google-cn,DIRECT",
     "GEOSITE,apple,DIRECT",
+    "GEOSITE,steam,DIRECT",
+    "GEOSITE,category-public-tracker,DIRECT",
     "GEOSITE,category-speedtest,DIRECT",
+    "GEOSITE,category-games,DIRECT",
+    "GEOSITE,bilibili,📺 哔哩哔哩",
+    "GEOSITE,github,🚀 节点选择",
     "GEOSITE,microsoft,DIRECT",
+    "GEOSITE,tld-!cn,🚀 节点选择",
+    "GEOSITE,geolocation-!cn,🚀 节点选择",
     "GEOSITE,cn,DIRECT",
-    "RULE-SET,telegram_ip,🚀 节点选择",
-    //////// 强制对域名进行DNS解析, 获取到IP之后在进行匹配 ////////
     //////////////////////////////////////////////////////////////
     "RULE-SET,reject_ip,REJECT",
-    //////// 禁用YouTube Quic ///////////////////////////////
-    "AND,(AND,(DST-PORT,443),(NETWORK,UDP)),(NOT,((GEOIP,cn))),REJECT",
-    //////////////////////////////////////////////////////////////
-    "GEOIP,private,DIRECT",
     "GEOIP,cn,DIRECT",
-    //////////////////////////////////////////////////////////////
-    // "DST-PORT,22,DIRECT",
-    // "DST-PORT,27017,DIRECT",
-    // "DST-PORT,3306,DIRECT",
-    //////////////////////////////////////////////////////////////
     "MATCH,🚀 节点选择",
   ];
 
@@ -209,22 +201,18 @@ function overwriteRules(config) {
 }
 
 function overwriteProxyGroups(config) {
-  const maxMultiple = 2;
-
-  const allProxyNames = config["proxies"].map((e) => e.name);
-  // 所有代理 过滤掉高倍率节点
+  const allProxyNames = config["proxies"].map((e) => e.name).filter((e) => e);
   const allAutoProxyNames = allProxyNames.filter((e) => {
-    // 倍率筛选
-    const regex = /【(\d+x)】/;
-    const match = e.match(regex);
-    if (match) {
-      const multiple = parseInt(match[1]);
-      return multiple <= maxMultiple;
-    }
-    return true;
+    const match = e.match(/ddns/);
+    return !match;
+    // const match = e.match(/【(\d+x)】/);
+    // if (match) {
+    //   const multiple = parseInt(match[1]);
+    //   return multiple <= maxMultiple;
+    // }
+    // return true;
   });
 
-  // 自动选择代理组
   const autoProxyGroupRegexs = [
     {
       name: "🇭🇰 HK-自动选择",
@@ -253,23 +241,33 @@ function overwriteProxyGroups(config) {
   ];
 
   const autoProxyGroups = autoProxyGroupRegexs
-    .map((item) => ({
-      name: item.name,
-      //////////////////////////////////////////////////////////
-      type: "url-test",
-      url: "http://www.gstatic.com/generate_204",
-      "expected-status": 204,
-      lazy: true,
-      interval: 300,
-      timeout: 5000,
-      "max-failed-times": 5,
-      hidden: true,
-      //////////////////////////////////////////////////////////
-      proxies: allAutoProxyNames.filter((e) => item.regex.test(e)),
-    }))
-    .filter((item) => item.proxies.length);
+    .map((item) => {
+      const proxies = allAutoProxyNames.filter((e) => item.regex.test(e));
+
+      return {
+        name: item.name,
+        //////////////////////////////////////////////////////////
+        type: "url-test",
+        url: "http://www.gstatic.com/generate_204",
+        "expected-status": 204,
+        lazy: true,
+        interval: 300,
+        timeout: 5000,
+        "max-failed-times": 5,
+        hidden: true,
+        //////////////////////////////////////////////////////////
+        proxies,
+      };
+    })
+    .filter((item) => item.proxies?.length > 0);
+  const autoProxyGroupNames = autoProxyGroups.map((item) => item.name);
 
   const proxyGroups = [
+    {
+      name: "📺 哔哩哔哩",
+      type: "select",
+      proxies: ["DIRECT", ...autoProxyGroupNames],
+    },
     {
       name: "🚀 节点选择",
       type: "select",
@@ -278,7 +276,7 @@ function overwriteProxyGroups(config) {
     {
       name: "🤖 自动选择",
       type: "select",
-      proxies: autoProxyGroups.map((item) => item.name),
+      proxies: autoProxyGroupNames,
     },
     {
       name: "🌴 手动选择",
@@ -289,7 +287,6 @@ function overwriteProxyGroups(config) {
 
   proxyGroups.push(...autoProxyGroups);
 
-  //////////////////////////////////////////////////////////////
   config["proxy-groups"] = proxyGroups;
 }
 
@@ -312,19 +309,19 @@ function overwriteOthers(config) {
       },
     },
     "skip-domain": [
-      // 米家设备
-      "Mijia Cloud",
-      // 向日葵
-      "+.oray.com",
-      "+.sunlogin.net",
-      // Apple
-      "+.apple.com",
+      "Mijia Cloud", // 米家设备
+      // "+.oray.com", // 向日葵
+      // "+.sunlogin.net", // 向日葵
+      // "+.apple.com", // Apple
     ],
   };
 
-  //////////////////////////////////////////////////////////////
-  config.ipv6 = true;
   config.sniffer = sniffer;
+
+  //////////////////////////////////////////////////////////////
+  config.mode = "rule";
+  config.ipv6 = true;
+  config["log-level"] = "info";
   config["tcp-concurrent"] = true;
   config["unified-delay"] = true;
   config["allow-lan"] = false;
@@ -335,10 +332,13 @@ function overwriteOthers(config) {
   config.profile = {
     "store-selected": true,
     "store-fake-ip": true,
+    // smart-collector-size: data collection file size, the default is 100 (MB)
+    "smart-collector-size": 100,
   };
 
   //////////////////////////////////////////////////////////////
   config["geodata-mode"] = true;
+  config["geodata-loader"] = "standard";
   config["geodata-auto-update"] = true;
   config["geodata-update-interval"] = 24;
   config["geox-url"] = {
@@ -350,4 +350,13 @@ function overwriteOthers(config) {
     asn: "https://fastly.jsdelivr.net/gh/xishang0128/geoip@release/GeoLite2-ASN.mmdb",
   };
   //////////////////////////////////////////////////////////////
+
+  // LightGBM Model
+  // enable model auto update, the default is false
+  config["lgbm-auto-update"] = true;
+  // model auto update interval, the default is 72 (hours)
+  config["lgbm-update-interval"] = 72;
+  // model update url
+  config["lgbm-url"] =
+    "https://github.com/vernesong/mihomo/releases/download/LightGBM-Model/Model.bin";
 }
