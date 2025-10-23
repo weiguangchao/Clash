c527d0a13c9af960428c581bc1b8d5515c6dbe87
wggc <3409885610@qq.com>
2025-10-23 09:32:12 +0800

    更新

--- clashverge_whitelist.js
+++ clashverge_whitelist.js
@@ -31,10 +31,10 @@ function overwriteDns(config) {
   const cnDohList = [
     "https://dns.alidns.com/dns-query", // 阿里云公共DNS
     "https://doh.pub/dns-query", // 腾讯DNSPod
-    "https://doh.360.cn/dns-query", // 360DNS
-    "https://doh.18bit.cn/dns-query", // 18Bit DNS
-    "https://dns.yuguan.xyz/dns-query", // 易安云DNS
-    "https://doh-pure.onedns.net/dns-query", // OneDNS
+    // "https://doh.360.cn/dns-query", // 360DNS
+    // "https://doh.18bit.cn/dns-query", // 18Bit DNS
+    // "https://dns.yuguan.xyz/dns-query", // 易安云DNS
+    // "https://doh-pure.onedns.net/dns-query", // OneDNS
   ];
 
   const gfwDnsList = [
@@ -69,7 +69,8 @@ function overwriteDns(config) {
       "geosite:connectivity-check",
       "geosite:private",
     ],
-    nameserver: [en0Dns],
+    "default-nameserver": cnDotList,
+    nameserver: cnDohList,
   };
 
   config.dns = dns;
@@ -284,7 +285,7 @@ function overwriteOthers(config) {
   config["allow-lan"] = false;
   config["disable-keep-alive"] = false;
   config["keep-alive-interval"] = 15;
-  config["keep-alive-idle"] = 300;
+  config["keep-alive-idle"] = 600;
   config.profile = {
     "store-selected": true,
     "store-fake-ip": true,
