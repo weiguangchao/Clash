6f68b38f33cb98b944eb347fd393802f0cd08362
wggc <3409885610@qq.com>
2026-06-09 09:35:23 +0800

    refactor: 将 GeneralClashConfig.yaml 配置重构到 clashverge_whitelist.js

--- clashverge_whitelist.js
+++ clashverge_whitelist.js
@@ -84,6 +84,13 @@ function overwriteProxyGroups(config) {
       "exclude-type": "direct",
     },
     {
+      name: "🎥 HBO",
+      type: "select",
+      proxies: ["🚀 节点选择"],
+      "include-all": true,
+      "exclude-type": "direct",
+    },
+    {
       name: "🐟 漏网之鱼",
       type: "select",
       proxies: ["🚀 节点选择"],
@@ -100,12 +107,6 @@ function overwriteProxyGroups(config) {
       proxies: ["REJECT-DROP"],
       hidden: true,
     },
-    {
-      name: "🛑 REJECT",
-      type: "select",
-      proxies: ["REJECT"],
-      hidden: true,
-    },
   ];
 
   config["proxy-groups"] = proxyGroups;
@@ -113,53 +114,179 @@ function overwriteProxyGroups(config) {
 
 function overwriteRules(config) {
   const ruleProviders = {
-    direct_classical_no_resolve: {
+    "fake-ip-filter": {
       type: "http",
-      behavior: "classical",
-      format: "yaml",
+      behavior: "domain",
+      format: "text",
+      url: "https://raw.githubusercontent.com/weiguangchao/Clash/refs/heads/master/fake-ip-filter.list",
       interval: 86400,
-      url: "https://raw.githubusercontent.com/weiguangchao/Clash/refs/heads/master/direct_classical_no_resolve.yaml",
     },
-    reject_drop_classical_no_resolve: {
+    "direct-classical-no-resolve": {
       type: "http",
       behavior: "classical",
       format: "yaml",
+      url: "https://raw.githubusercontent.com/weiguangchao/Clash/refs/heads/master/direct-classical-no-resolve.yaml",
       interval: 86400,
-      url: "https://raw.githubusercontent.com/weiguangchao/Clash/refs/heads/master/reject_drop_classical_no_resolve.yaml",
     },
-    reject_classical_no_resolve: {
+    "geosite-category-ai-!cn": {
       type: "http",
-      behavior: "classical",
-      format: "yaml",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-ai-!cn.mrs",
       interval: 86400,
-      url: "https://raw.githubusercontent.com/weiguangchao/Clash/refs/heads/master/reject_classical_no_resolve.yaml",
+    },
+    "geosite-cursor": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/cursor.mrs",
+      interval: 86400,
+    },
+    "geosite-google": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/google.mrs",
+      interval: 86400,
+    },
+    "geosite-private": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/private.mrs",
+      interval: 86400,
+    },
+    "geosite-google-cn": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/google-cn.mrs",
+      interval: 86400,
+    },
+    "geosite-category-public-tracker": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-public-tracker.mrs",
+      interval: 86400,
+    },
+    "geosite-category-cryptocurrency": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-cryptocurrency.mrs",
+      interval: 86400,
+    },
+    "geosite-apple": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/apple.mrs",
+      interval: 86400,
+    },
+    "geosite-category-games@cn": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-games@cn.mrs",
+      interval: 86400,
+    },
+    "geosite-category-games": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-games.mrs",
+      interval: 86400,
+    },
+    "geosite-bilibili": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/bilibili.mrs",
+      interval: 86400,
+    },
+    "geosite-category-speedtest": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/category-speedtest.mrs",
+      interval: 86400,
+    },
+    "geosite-hbo": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/hbo.mrs",
+      interval: 86400,
+    },
+    "geosite-youtube": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/youtube.mrs",
+      interval: 86400,
+    },
+    "geosite-github": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/github.mrs",
+      interval: 86400,
+    },
+    "geosite-microsoft": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/microsoft.mrs",
+      interval: 86400,
+    },
+    "geosite-cn": {
+      type: "http",
+      behavior: "domain",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/cn.mrs",
+      interval: 86400,
+    },
+    "geoip-private": {
+      type: "http",
+      behavior: "ipcidr",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/private.mrs",
+      interval: 86400,
+    },
+    "geoip-cn": {
+      type: "http",
+      behavior: "ipcidr",
+      format: "mrs",
+      url: "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/cn.mrs",
+      interval: 86400,
     },
   };
 
   const rules = [
-    "RULE-SET,reject_classical_no_resolve,🛑 REJECT",
-    "RULE-SET,reject_drop_classical_no_resolve,🛑 DROP",
-    "RULE-SET,direct_classical_no_resolve,🎯 全球直连",
-    "GEOSITE,private,🎯 全球直连",
-    "GEOSITE,connectivity-check,🎯 全球直连",
-    "GEOSITE,google-cn,🎯 全球直连",
-    "GEOSITE,category-public-tracker,🎯 全球直连",
-    "GEOSITE,category-games@cn,🎯 全球直连",
-    "GEOSITE,category-cryptocurrency,🎯 全球直连",
-    "GEOSITE,apple,🍎 Apple",
-    "GEOSITE,notion,🎯 全球直连",
-    "GEOSITE,category-games,🎮 Game",
-    "GEOSITE,bilibili,📺 Bilibili",
-    "GEOSITE,category-speedtest,⏱️ Speedtest",
-    "GEOSITE,category-ai-!cn,🤖 AI",
-    "GEOSITE,cursor,🤖 AI",
-    "GEOSITE,youtube,📹 YouTube",
-    "GEOSITE,google,🇬 Google",
-    "GEOSITE,github,🚀 节点选择",
-    "GEOSITE,microsoft,Ⓜ️ Microsoft",
-    "GEOSITE,cn,🎯 全球直连",
-    "GEOIP,private,🎯 全球直连,no-resolve",
-    "GEOIP,cn,🎯 全球直连,no-resolve",
+    "AND,((RULE-SET,geosite-category-ai-!cn),(NETWORK,UDP)),🛑 DROP",
+    "AND,((RULE-SET,geosite-cursor),(NETWORK,UDP)),🛑 DROP",
+    "AND,((RULE-SET,geosite-google),(NETWORK,UDP)),🛑 DROP",
+    "RULE-SET,direct-classical-no-resolve,🎯 全球直连",
+    "RULE-SET,geosite-private,🎯 全球直连",
+    "RULE-SET,geosite-google-cn,🎯 全球直连",
+    "RULE-SET,geosite-category-public-tracker,🎯 全球直连",
+    "RULE-SET,geosite-category-cryptocurrency,🎯 全球直连",
+    "RULE-SET,geosite-apple,🍎 Apple",
+    "RULE-SET,geosite-category-games@cn,🎯 全球直连",
+    "RULE-SET,geosite-category-games,🎮 Game",
+    "RULE-SET,geosite-bilibili,📺 Bilibili",
+    "RULE-SET,geosite-category-speedtest,⏱️ Speedtest",
+    "RULE-SET,geosite-hbo,🎥 HBO",
+    "RULE-SET,geosite-category-ai-!cn,🤖 AI",
+    "RULE-SET,geosite-cursor,🤖 AI",
+    "RULE-SET,geosite-youtube,📹 YouTube",
+    "RULE-SET,geosite-google,🇬 Google",
+    "RULE-SET,geosite-github,🚀 节点选择",
+    "RULE-SET,geosite-microsoft,Ⓜ️ Microsoft",
+    "RULE-SET,geosite-cn,🎯 全球直连",
+    "RULE-SET,geoip-private,🎯 全球直连,no-resolve",
+    "RULE-SET,geoip-cn,🎯 全球直连,no-resolve",
     "MATCH,🐟 漏网之鱼",
   ];
 
@@ -178,17 +305,7 @@ function overwriteDns(config) {
     "enhanced-mode": "fake-ip",
     "fake-ip-range": "198.18.0.1/16",
     "fake-ip-filter-mode": "blacklist",
-    "fake-ip-filter": [
-      "GEOSITE:private",
-      "GEOSITE:connectivity-check",
-      "+.localhost.work.weixin.qq.com",
-      "stun.*.*",
-      "stun.*.*.*",
-      "+.stun.*.*",
-      "+.stun.*.*.*",
-      "+.stun.*.*.*.*",
-      "+.stun.*.*.*.*.*",
-    ],
+    "fake-ip-filter": ["rule-set:fake-ip-filter"],
     nameserver: ["223.5.5.5", "119.29.29.29", "114.114.114.114"],
   };
 
@@ -200,10 +317,10 @@ function overwriteSniffer(config) {
     enable: true,
     "force-dns-mapping": true,
     "parse-pure-ip": true,
-    "override-destination": true,
+    "override-destination": false,
     sniff: {
       QUIC: {
-        ports: [443],
+        ports: [443, 8443],
       },
       TLS: {
         ports: [443, 8443],
@@ -225,6 +342,13 @@ function overwriteSniffer(config) {
       "+.oray.com",
       "+.sunlogin.net",
       "+.push.apple.com",
+    ],
+    "skip-dst-address": [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+      "127.0.0.0/8",
+      "224.0.0.0/4",
     ],
   };
 
@@ -256,7 +380,7 @@ function overwriteOthers(config) {
   config["unified-delay"] = true;
   config["disable-keep-alive"] = false;
   config["keep-alive-interval"] = 15;
-  config["keep-alive-idle"] = 300;
+  config["keep-alive-idle"] = 600;
   config.profile = {
     "store-selected": true,
     "store-fake-ip": true,
