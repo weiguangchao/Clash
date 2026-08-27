38208a7f3f621fb465a84c3295ea0c656430eb45
wggc <3409885610@qq.com>
2026-08-27 16:25:15 +0800

    feat: add 'find-process-mode' configuration to clashverge_whitelist.js, GeneralClashConfig.yaml, and openclash_whitelist.conf for enhanced process management

--- clashverge_whitelist.js
+++ clashverge_whitelist.js
@@ -308,6 +308,7 @@ function overwriteOthers(config) {
   config["disable-keep-alive"] = false;
   config["keep-alive-interval"] = 15;
   config["keep-alive-idle"] = 300;
+  config["find-process-mode"] = "off";
   config.profile = {
     "store-selected": true,
     "store-fake-ip": true,
